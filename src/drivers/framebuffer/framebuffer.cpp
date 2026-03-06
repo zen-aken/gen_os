8fc55298f9a197f1a090c604219cc3c4cec2e278
zen-aken <arslanahmet4772@gmail.com>
2026-03-06 23:37:46 +0100

    Clear start for limine-protocol

--- src/drivers/framebuffer/framebuffer.cpp
+++
@@ -1,9 +0,0 @@
-#include <boot/multiboot2.h>
-#include <drivers/framebuffer.h>
-
-multiboot_tag_framebuffer *Framebuffer::framebuffer_info = nullptr;
-
-void Framebuffer::set_framebuffer_info(multiboot_tag_framebuffer *_framebuffer_info)
-{
-    framebuffer_info = _framebuffer_info;
-}
--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,11 +1,16 @@
 #include <stdint.h>
-#include <boot/multiboot2.h>
-#include <mm/PhysicalMM.h>
+#include <limine.h>
 
-extern "C" void kernel_main(uintptr_t magic, uintptr_t mbi)
+__attribute__((used, section(".limine_requests"))) static volatile limine_framebuffer_request fb_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST_ID,
+    .revision = 0};
+
+__attribute__((used, section(".limine_requests_start"))) static volatile uint64_t limine_start[] = LIMINE_REQUESTS_START_MARKER;
+
+__attribute__((used, section(".limine_requests_end"))) static volatile uint64_t limine_end[] = LIMINE_REQUESTS_END_MARKER;
+
+extern "C" void kernel_main(void)
 {
-    Multiboot2::init(magic, mbi);
-    PhysicalMM::init();
     while (1)
     {
         asm volatile("hlt");
